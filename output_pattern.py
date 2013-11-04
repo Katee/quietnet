081a5307fc08fe358a85d4bc0580f8faab61f968
katee <hello@kate.io>
2013-11-04 14:54:18 -0500

    Adds basic test pattern program and a python notebook of inprogress work cleaning up the signal.

---
+++ output_pattern.py
@@ -0,0 +1,80 @@
+import sys
+import pyaudio
+from datetime import datetime, timedelta
+import quietnet
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+
+FREQ_OFF = 0
+FREQ = 19043
+FRAME_LENGTH = 8
+DATASIZE = 44 * FRAME_LENGTH
+
+SIGIL = "0001111111111000"
+
+buffers = {
+    "000": quietnet.tone(FREQ_OFF, DATASIZE),
+    "100": quietnet.tone(FREQ_OFF, DATASIZE),
+    "001": quietnet.tone(FREQ_OFF, DATASIZE),
+    "101": quietnet.tone(FREQ_OFF, DATASIZE),
+    "010": quietnet.envelope(quietnet.tone(FREQ, DATASIZE)),
+    "011": quietnet.lenvelope(quietnet.tone(FREQ, DATASIZE)),
+    "110": quietnet.renvelope(quietnet.tone(FREQ, DATASIZE)),
+    "111": quietnet.tone(FREQ, DATASIZE)
+}
+
+def make_buffer_from_bit_pattern(pattern):
+    last_bit = pattern[-1]
+    output_buffer = []
+    for i in range(len(pattern)):
+        bit = pattern[i]
+        if i < len(pattern) - 1:
+            next_bit = pattern[i+1]
+        else:
+            next_bit = pattern[0]
+        output_buffer = output_buffer + buffers[last_bit + bit + next_bit]
+        last_bit = bit
+    return quietnet.pack_buffer(output_buffer)
+
+def repeat_buffer(buffer):
+    while True:
+        for sample in buffer:
+            stream.write(sample)
+
+def play_tone():
+    buffer = quietnet.pack_buffer(quietnet.tone(FREQ, DATASIZE))
+    repeat_buffer(buffer)
+
+def fill(letter):
+    n = ord(letter)
+    b = str(bin(n))[2:]
+    return b.zfill(8)
+
+def make_bit_pattern_from_string(string, prefix):
+    pattern = prefix
+    for l in string:
+        pattern += fill(l)
+    return pattern
+
+if len(sys.argv) > 1:
+    pattern = sys.argv[1]
+else:
+    print "enter a string to repeat:",
+    pattern = make_bit_pattern_from_string(raw_input(), SIGIL)
+
+print "repeating %s" % pattern
+print "Use ctrl-c to exit"
+buffer = make_buffer_from_bit_pattern(pattern)
+
+# keep playing the buffer until the program is terminated
+repeat_buffer(buffer)
+
+# never actually reached, right now you exit with ctrl-c
+stream.stop_stream()
+stream.close()
+p.terminate()
--- quietnet.py
+++ quietnet.py
@@ -21,12 +21,12 @@ def fft(signal):
 def get_peak(hertz, rate, chunk):
     return int(round((float(hertz) / rate) * chunk))
 
-def has_freq(fft_sample, freq_in_hertz, rate, chunk):
-    offset = 2
+def has_freq(fft_sample, freq_in_hertz, rate, chunk, offset=3):
     peak = get_peak(freq_in_hertz, rate, chunk)
-    mid = fft_sample[peak]
-    threshold = 10000 # this should be based on the sample size
-    return mid > threshold and mid > fft_sample[peak - offset] and mid > fft_sample[peak + offset]
+    top = max(fft_sample[peak-1:peak+2])
+    if top < np.average([fft_sample[peak-offset], fft_sample[peak+offset]]):
+        return 0
+    return fft_sample[peak]
 
 def get_signal(buffer):
     unpacked_buffer = unpack_buffer(list(chunks(buffer, 2)))
@@ -36,6 +36,49 @@ def raw_has_freq(buffer, freq_in_hertz, rate, chunk):
     fft_sample = fft(get_signal(buffer))
     return has_freq(fft_sample, freq_in_hertz, rate, chunk)
 
+def get_freq_over_time(ffts, search_freq, chunk=1024, rate=44100):
+    return [has_freq(fft, search_freq, rate, chunk) for fft in ffts]
+
+def get_points(freq_values, threshold=None):
+    if threshold == None:
+        max = np.max(freq_values)
+        threshold = (np.max(freq_values) + np.min(freq_values)) / 2
+    points = []
+    for point in freq_values:
+        if point < threshold:
+            points.append(0)
+        else:
+            points.append(1)
+
+    # TODO is this a hack for framing?
+    i = 0
+    while i < len(points):
+        if points[i] != points[i+1]:
+            points = points[i:]
+            break
+        i += 1
+    return points
+
+def get_bits(points, frame_length):
+    return [int(round(sum(c) / float(frame_length))) for c in list(chunks(points, frame_length)) if len(c) == frame_length]
+
+def get_bytes(bits, sigil):
+    i = 0
+    # scan for the first occurance of the sigil
+    while i < len(bits) - len(sigil):
+        if bits[i:i+len(sigil)] == sigil:
+            i += len(sigil)
+            break
+        i += 1
+    return [l for l in list(chunks(bits[i:], 8)) if len(l) == 8]
+
+def decode(bytes):
+    string = ""
+    for byte in bytes:
+        byte = ''.join([str(bit) for bit in byte])
+        string += chr(int(byte, base=2))
+    return string
+
 def tone(freq=400, datasize=4096, rate=44100):
     amp=8000.0
     sine_list=[]
@@ -43,3 +86,35 @@ def tone(freq=400, datasize=4096, rate=44100):
         samp = math.sin(2*math.pi*freq*(x/float(rate)))
         sine_list.append(int(samp*amp/2))
     return sine_list
+
+def envelope(in_data, rate=44100):
+    freq = math.pi / len(in_data)
+    amp=8000.0
+    sine_list=[]
+    for x in range(len(in_data)):
+        samp = math.sin(freq*x) * in_data[x]
+        sine_list.append(int(samp))
+    return sine_list
+
+def lenvelope(in_data, rate=44100):
+    half = float(len(in_data)) / 2
+    freq = math.pi / len(in_data)
+    out_data=[]
+    for x in range(len(in_data)):
+        samp = in_data[x]
+        if x < half:
+            samp = samp * math.sin(freq*x)
+        out_data.append(int(samp))
+    return out_data
+
+def renvelope(in_data, rate=44100):
+    half = int(float(len(in_data)) / 2)
+    freq = math.pi / len(in_data)
+    out_data=[]
+    for x in range(len(in_data)):
+        samp = in_data[x]
+        if x > half:
+            samp = samp * math.sin(freq*x)
+        out_data.append(int(samp))
+    return out_data
+
