d5c99be8022ffa830534e5e8a537cdc86a506867
katee <hello@kate.io>
2014-01-11 16:16:55 -0500

    Updates options use a chunk size of 256 and a frame_length of 3. This makes things a bit faster and still seemed to work well.

--- options.py
+++ options.py
@@ -1,7 +1,7 @@
 rate = 44100
-freq = 19043# chosen because it is outside most people's hearing but can be picked up by many laptop speakers
+freq = 19100# chosen because it is outside most people's hearing but can be picked up by many laptop speakers
 channels = 1
-frame_length = 4
-chunk = 44 * 8
+frame_length = 3
+chunk = 256
 datasize = chunk * frame_length
 sigil = "00"
