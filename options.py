5d446bc800eebc59e3c9bd4295894e20ff7cf924
katee <hello@kate.io>
2014-01-11 15:53:40 -0500

    Adds notes about why a frequency around 19kHz was chosen.

--- options.py
+++ options.py
@@ -1,5 +1,5 @@
 rate = 44100
-freq = 19043
+freq = 19043# chosen because it is outside most people's hearing but can be picked up by many laptop speakers
 channels = 1
 frame_length = 4
 chunk = 44 * 8
