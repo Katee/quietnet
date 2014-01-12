20377c278d14a4af44c51b3fdcee6b457b1ea93e
katee <hello@kate.io>
2014-01-12 13:18:36 -0500

    Fixes comment on why freq was chosen, as tialaramex noted it made no sense.

--- options.py
+++ options.py
@@ -1,5 +1,5 @@
 rate = 44100
-freq = 19100# chosen because it is outside most people's hearing but can be picked up by many laptop speakers
+freq = 19100# chosen because it is outside most people's hearing and worked for my microphone and speakers
 channels = 1
 frame_length = 3
 chunk = 256
